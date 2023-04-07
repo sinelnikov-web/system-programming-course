079b65815136e23fefee190ffe5e0de7aaea3794
Максим <sinelnikov-web@mail.ru>
2023-04-07 12:05:36 +0200

    Create DateTimePrinter dll and create DateTimePrinter program

---
+++ DatePrinterProgram/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+using SystemProgramming;
+
+namespace DatePrinterProgram;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите дату в формате DD.MM.YYYY: ");
+        string dateString = Console.ReadLine();
+        DateTime date;
+        DateTimeFormatInfo dtfi = CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat;
+        DateTime.TryParse(dateString, dtfi, out date);
+        DateTimePrinter printer = new DateTimePrinter(date);
+        while (true)
+        {
+            Console.WriteLine("1 - Задать новую дату");
+            Console.WriteLine("2 - Напечатать дату");
+            Console.WriteLine("3 - Напечатать год");
+            Console.WriteLine("4 - Напечатать месяц");
+            Console.WriteLine("5 - Напечатать день недели");
+            Console.WriteLine("6 - Завершить программу");
+            string command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "1":
+                    {
+                        Console.WriteLine("Введите дату в формате DD.MM.YYYY: ");
+                        dateString = Console.ReadLine();
+                        DateTime.TryParse(dateString, dtfi, out date);
+                        printer = new DateTimePrinter(date);
+                        break;
+                    }
+                case "2":
+                    {
+                        printer.printDate();
+                        break;
+                    }
+                case "3":
+                    {
+                        printer.printYear();
+                        break;
+                    }
+                case "4":
+                    {
+                        printer.printMonth();
+                        break;
+                    }
+                case "5":
+                    {
+                        printer.printWeekDay();
+                        break;
+                    }
+                case "6":
+                    {
+                        return;
+                    }
+            }
+           
+        }
+    }
+}
--- SystemProgramming/Class1.cs
+++ SystemProgramming/Class1.cs
@@ -1,6 +1,36 @@
-﻿namespace SystemProgramming;
-public class Class1
+﻿using System.Globalization;
+
+namespace SystemProgramming;
+public class DateTimePrinter
 {
 
+    //1.4.3 Разработать DLL для работы с переменными типа DateTime.В библиотеку
+    //включить методы, позволяющие по значению заданной переменной типа
+    //DateTime отдельно печатать значение года, месяца и дня недели. В программе
+    //предусмотреть печать результатов после выполнения любого метода DLL.
+    //Значение переменных задавать в режиме диалога.
+
+    private DateTime date;
+
+    public DateTimePrinter(DateTime date)
+    {
+        this.date = date;
+    }
+    public void printDate()
+    {
+        Console.WriteLine(this.date.Date.ToString("dd.MM.yyyy"));
+    }
+    public void printYear()
+    {
+        Console.WriteLine(this.date.Year);
+    }
+    public void printMonth()
+    {
+        Console.WriteLine(this.date.Month);
+    }
+    public void printWeekDay()
+    {
+        Console.WriteLine(this.date.DayOfWeek);
+    }
 }
 
