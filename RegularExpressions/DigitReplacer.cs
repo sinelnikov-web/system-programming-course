45a45d1f52071e58bce523132b7b9017b88fa7a3
Максим <sinelnikov-web@mail.ru>
2023-04-15 00:15:18 +0600

    Digit Replacer and homework

---
+++ RegularExpressions/DigitReplacer.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpressions;
+class DigitReplacer
+{
+    private Dictionary<string, string> numbers = new Dictionary<string, string>()
+    {
+        { "0", "Ноль" },
+        { "1", "Один" },
+        { "2", "Два" },
+        { "3", "Три" },
+        { "4", "Четыре" },
+        { "5", "Пять" },
+        { "6", "Шесть" },
+        { "7", "Семь" },
+        { "8", "Восемь" },
+        { "9", "Девять" },
+    };
+    public string replace(string text)
+    {
+        Regex regex = new Regex(@"\d");
+        string newText = regex.Replace(text, delegate (Match match)
+        {
+            string v = match.ToString();
+            return this.numbers[v];
+        });
+        return newText;
+    }
+}
+
---
+++ RegularExpressions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpressions;
+class Program
+{
+    static void Main()
+    {
+        // Домашнее задание
+        Regex regex = new Regex(@"(?<=^|\s)\S+(?=$|\s)");
+        string text = File.ReadAllText("TextFile.txt");
+        string[] sentences = text.Split(". ");
+        int[] wordsCountBySentence = sentences.Select(sentence => regex.Matches(sentence).ToArray().Length).ToArray();
+        Console.WriteLine(String.Join(" ", wordsCountBySentence));
+        Console.WriteLine("Введите кол-во слов: ");
+        int wordsCount = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < sentences.Length; i++)
+        {
+            if (wordsCountBySentence[i] == wordsCount)
+            {
+                Console.WriteLine(sentences[i]);
+            }
+        }
+
+
+        // Индивидуальное задание
+        DigitReplacer replacer = new DigitReplacer();
+        text = File.ReadAllText("TextWithDigits.txt");
+        string newText = replacer.replace(text);
+        Console.WriteLine("Изначальный текст:\n");
+        Console.WriteLine(text);
+        Console.WriteLine("Изменённый текст:\n");
+        Console.WriteLine(newText);
+        Console.ReadKey();
+    }
+}
